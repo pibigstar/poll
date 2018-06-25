4dc4d81b246ef8437df7988ab5325411f16e193e
pibigstar <leikewei123@163.com>
2018-06-25 19:14:58 +0800

    文件上传示例

--- src/main/java/com/pibigstar/web/FileController.java
+++ src/main/java/com/pibigstar/web/FileController.java
@@ -2,7 +2,11 @@ package com.pibigstar.web;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,8 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.pibigstar.utils.FastDFSFile;
-import com.pibigstar.utils.FileUtil;
+import com.pibigstar.common.Constant;
 
 /**
  * 文件上传Controller
@@ -23,65 +26,36 @@ import com.pibigstar.utils.FileUtil;
 @RequestMapping("/file")
 public class FileController extends BaseController{
 	
-	@GetMapping("toUpload")
-	public String toUpload() {
-		return "upload";
-	}
-	
-	
-	@PostMapping("upload") //new annotation since 4.3
-	public String singleFileUpload(@RequestParam("file") MultipartFile file,
-			RedirectAttributes redirectAttributes) {
-		if (file.isEmpty()) {
-			redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
-			return "redirect:/file/uploadStatus";
-		}
-		try {
-			// Get the file and save it somewhere
-			String path=saveFile(file);
-			redirectAttributes.addFlashAttribute("message",
-					"You successfully uploaded '" + file.getOriginalFilename() + "'");
-			redirectAttributes.addFlashAttribute("path",
-					"file path url '" + path + "'");
-		} catch (Exception e) {
-			logger.error("upload file failed",e);
-		}
-		return "redirect:/file/uploadStatus";
-	}
+    private final String upload_path = Constant.DEFAULT_FILE_UPLOAD_PATH;
 
-	@GetMapping("uploadStatus")
-	public String uploadStatus() {
-		return "uploadStatus";
-	}
+    @GetMapping("toUpload")
+    public String index() {
+        return "upload";
+    }
 
-	/**
-	 * @param multipartFile
-	 * @return
-	 * @throws IOException
-	 */
-	public String saveFile(MultipartFile multipartFile) throws IOException {
-		String[] fileAbsolutePath={};
-		String fileName=multipartFile.getOriginalFilename();
-		String ext = fileName.substring(fileName.lastIndexOf(".") + 1);
-		byte[] file_buff = null;
-		InputStream inputStream=multipartFile.getInputStream();
-		if(inputStream!=null){
-			int len1 = inputStream.available();
-			file_buff = new byte[len1];
-			inputStream.read(file_buff);
-		}
-		inputStream.close();
-		FastDFSFile file = new FastDFSFile(fileName, file_buff, ext);
-		try {
-			fileAbsolutePath = FileUtil.upload(file);  //upload to fastdfs
-		} catch (Exception e) {
-			logger.error("upload file Exception!",e);
-		}
-		if (fileAbsolutePath==null) {
-			logger.error("upload file failed,please upload again!");
-		}
-		String path=FileUtil.getTrackerUrl()+fileAbsolutePath[0]+ "/"+fileAbsolutePath[1];
-		return path;
-	}
+    @PostMapping("upload")
+    public String singleFileUpload(@RequestParam("file") MultipartFile file,RedirectAttributes redirectAttributes) {
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "请选择文件进行上传");
+            return "redirect:/file/uploadStatus";
+        }
 
+        try {
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(upload_path+file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            redirectAttributes.addFlashAttribute("message","成功上传文件： '" + file.getOriginalFilename() + "'");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "redirect:/file/uploadStatus";
+    }
+
+    @GetMapping("uploadStatus")
+    public String uploadStatus() {
+        return "uploadStatus";
+    }
 }
