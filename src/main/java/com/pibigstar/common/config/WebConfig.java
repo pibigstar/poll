8591a75428414f1700cb033a9c2e58cc05147b18
pibigstar <leikewei123@163.com>
2018-06-26 16:14:29 +0800

    增加文件上传功能

--- src/main/java/com/pibigstar/common/config/WebConfig.java
+++ src/main/java/com/pibigstar/common/config/WebConfig.java
@@ -6,10 +6,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
-	
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        //registry.addResourceHandler("/**").addResourceLocations("classpath:/statics/");
-    }
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		//registry.addResourceHandler("/**").addResourceLocations("classpath:/statics/");
+		//配置server虚拟路径，handler为jsp中访问的目录，locations为files相对应的本地路径     
+		registry.addResourceHandler("/files/**").addResourceLocations("file:///D:/upload/");  
+	}
 
 }
--- src/main/java/com/pibigstar/web/FileController.java
+++ src/main/java/com/pibigstar/web/FileController.java
@@ -1,12 +1,19 @@
 package com.pibigstar.web;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.springframework.beans.factory.annotation.Value;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,37 +32,81 @@ import com.pibigstar.common.Constant;
 @Controller()
 @RequestMapping("/file")
 public class FileController extends BaseController{
-	
-    private final String upload_path = Constant.DEFAULT_FILE_UPLOAD_PATH;
 
-    @GetMapping("toUpload")
-    public String index() {
-        return "upload";
-    }
+	private final String upload_path = Constant.DEFAULT_FILE_UPLOAD_PATH;
 
-    @PostMapping("upload")
-    public String singleFileUpload(@RequestParam("file") MultipartFile file,RedirectAttributes redirectAttributes) {
-        if (file.isEmpty()) {
-            redirectAttributes.addFlashAttribute("message", "请选择文件进行上传");
-            return "redirect:/file/uploadStatus";
-        }
+	@GetMapping("toUpload")
+	public String index() {
+		return "upload";
+	}
 
-        try {
-            byte[] bytes = file.getBytes();
-            Path path = Paths.get(upload_path+file.getOriginalFilename());
-            Files.write(path, bytes);
+	/**
+	 * 上传
+	 * @param file
+	 * @param redirectAttributes
+	 * @return
+	 */
+	@PostMapping("upload")
+	public String singleFileUpload(@RequestParam("file") MultipartFile file,RedirectAttributes redirectAttributes) {
+		if (file.isEmpty()) {
+			redirectAttributes.addFlashAttribute("message", "请选择文件进行上传");
+			return "redirect:/file/uploadStatus";
+		}
 
-            redirectAttributes.addFlashAttribute("message","成功上传文件： '" + file.getOriginalFilename() + "'");
+		try {
+			byte[] bytes = file.getBytes();
+			Path path = Paths.get(upload_path+file.getOriginalFilename());
+			Files.write(path, bytes);
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+			redirectAttributes.addFlashAttribute("message","成功上传文件： '" + file.getOriginalFilename() + "'");
 
-        return "redirect:/file/uploadStatus";
-    }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-    @GetMapping("uploadStatus")
-    public String uploadStatus() {
-        return "uploadStatus";
-    }
+		return "redirect:/file/uploadStatus";
+	}
+
+	/**
+	 * 下载
+	 * @param res
+	 * @throws IOException
+	 */
+	@GetMapping("download")  
+	public void download(HttpServletResponse res) throws IOException {
+
+		String fileName = "Main.jpg";
+		res.setHeader("content-type", "application/octet-stream");
+		res.setContentType("application/octet-stream");
+		res.setHeader("Content-Disposition", "attachment;filename=" + fileName);
+		byte[] buff = new byte[1024];
+		BufferedInputStream bis = null;
+		OutputStream os = null;
+		try {
+			os = res.getOutputStream();
+			bis = new BufferedInputStream(new FileInputStream(new File("E://temp/"+ fileName)));
+			int i = bis.read(buff);
+			while (i != -1) {
+				os.write(buff, 0, buff.length);
+				os.flush();
+				i = bis.read(buff);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (bis != null) {
+				try {
+					bis.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		System.out.println("success");
+	}
+
+	@GetMapping("uploadStatus")
+	public String uploadStatus() {
+		return "uploadStatus";
+	}
 }
